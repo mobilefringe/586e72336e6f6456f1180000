bce9a756792aa80c9da2bc757c3efed5ffdf22ec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-12 22:08:30 -0400

    Londonderry V2 2.5  /application.js

--- application.js
+++ application.js
@@ -121,7 +121,9 @@ function renderPromotions(container, template, collection, centre){
             val.store_name = store_details.name;
             val.store_show = "display:inline-block";
             val.image_url = val.promo_image_url_abs;
-            val.cat_list = store_details.categories.join(',')
+            if(store_details.categories != null){
+                val.cat_list = store_details.categories.join(',')
+            }
             val.store_slug = "/stores/" + store_details.slug
         }
         else{
@@ -227,7 +229,9 @@ function renderEvents(container, template, collection, centre){
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
             val.image_url = store_details.store_front_url_abs;
-            val.cat_list = store_details.categories.join(',')
+            if(store_details.categories != null){    
+                val.cat_list = store_details.categories.join(',')
+            }
         }
         else {
             val.store_name = centre;
